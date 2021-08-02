7862f550c495e8a3fe6bb2a057e43b2d7d005d16
nogw <gabrielnogueiraoliveira@gmail.com>
2021-08-01 21:06:21 -0400

    reuse module

---
+++ modules/operations.ml
@@ -0,0 +1,15 @@
+module Operations = struct
+  let sum a b = 
+    a + b
+  ;;
+  let minus a b =
+    a - b
+  ;;
+  let mult a b =
+    a * b
+  ;;
+
+  let div a b =
+    a / b
+  ;;
+end
---
+++ modules/reuse.ml
@@ -0,0 +1,6 @@
+open Operations
+
+let _ = 
+  Operations.sum 1 2
+  |> print_int
+;;
