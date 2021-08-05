a333a0231386fb06981fb9271abec0fdecf10dac
nogw <gabrielnogueiraoliveira@gmail.com>
2021-08-04 21:21:30 -0400

    i started studying ocaml throught cs3110

---
+++ cs3110/variants/_build/default/variants.ml
@@ -0,0 +1,22 @@
+type day = 
+  | Sun 
+  | Mon 
+  | Tue 
+  | Wed 
+  | Thu 
+  | Fri 
+  | Sat
+
+let int_of_day d = 
+  match d with
+  | Sun -> 1
+  | Mon -> 2
+  | Tue -> 3
+  | Wed -> 4
+  | Thu -> 5
+  | Fri -> 6
+  | Sat -> 7
+
+let _ = 
+  print_int (int_of_day Wed)
+;;
