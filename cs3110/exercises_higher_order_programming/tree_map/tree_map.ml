76696a8ff506a45bcdc4702aeaba45a851e354f0
nogw <gabrielnogueiraoliveira@gmail.com>
2021-08-17 21:59:28 -0300

    map tree

---
+++ cs3110/exercises_higher_order_programming/tree_map/tree_map.ml
@@ -0,0 +1,25 @@
+type 'a node = 
+  | Leaf 
+  | Node of 'a * 'a node * 'a node
+
+let rec map_tree f t =
+  match t with
+  | Leaf -> Leaf
+  | Node (v, l ,r) -> Node(f(v), map_tree f l, map_tree f r)
+
+let rec print_tree t = 
+  match t with
+  | Leaf -> "Leaf"
+  | Node (v, l, r) -> Format.sprintf "Node(%d, %s, %s)" v (print_tree l) (print_tree r)
+
+let _ =
+  let t = 
+    Node(3,
+      Node(2, Leaf, Leaf),
+      Node(1, Leaf, Leaf)
+    )
+  and square = fun x -> x * 2
+  in (
+    print_endline (print_tree t),
+    print_endline (print_tree (map_tree square t)) 
+  )
