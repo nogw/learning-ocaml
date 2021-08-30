0e285a539c8ceb2baf5fd262513f3fa45719733a
nogw <gabrielnogueiraoliveira@gmail.com>
2021-08-29 21:56:58 -0300

    direct address tables

--- direct_address_tables/direct_address_tables.ml
+++ direct_address_tables/direct_address_tables.ml
@@ -1,18 +1,37 @@
-(* module ListMap: Map = struct *)
-module ListMap = struct
-  type ('k, 'v) t = ('k * 'v) list
-  let insert k v m =
-    (k, v) :: m
-  let find = List.assoc_opt
-  let remove k lst =
-    List.filter (fun (k', _) -> k <> k') lst  
-  let empty = []
-  let of_list lst =
-    lst
-  let keys m =
-    m |> List.map fst |> List.sort_uniq Stdlib.compare
-  let binding m k =
-    (k, List.assoc k m)
-  let bindings m =
-    List.map (binding m) (keys m)
+module type DirectAddressMap = sig
+  type 'v t
+  val insert : int -> 'v -> 'v t -> unit
+  val find : int -> 'v t -> 'v option
+  val remove : int -> 'v t -> unit
+  val create : int -> 'v t
+  val of_list : int -> (int * 'v) list -> 'v t
+  val bindings : 'v t -> (int * 'v) list
+end
+
+(* Here is an implementation of that interface *)
+
+module ArrayMap : DirectAddressMap = struct
+  type 'v t = 'v option array
+  let insert k v a =
+    a.(k) <- Some v
+  let find k a =
+    a.(k)
+  let remove k a =
+    a.(k) <- None
+
+  let create c =
+    Array.make c None
+  let of_list c lst =
+    let a = create c in 
+    List.iter (fun (k, v) -> insert k v a) lst;
+    a
+  let bindings a =
+    let bs = ref [] in
+    let add_binding k v = 
+      match v with
+      | None -> ()
+      | Some v -> bs := (k, v) :: !bs
+    in
+      Array.iteri add_binding a;
+    !bs
 end
