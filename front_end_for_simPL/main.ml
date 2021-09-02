f9453d063b5932a9da1dd1fa8b2d48c7d6fa74c8
nogw <gabrielnogueiraoliveira@gmail.com>
2021-09-01 22:29:33 -0300

    substitution in simpl

--- front_end_for_simPL/main.ml
+++ front_end_for_simPL/main.ml
@@ -5,6 +5,89 @@ let parse (s: string) : expr =
   let ast = Parser.prog Lexer.read lexbuf in
   ast
   
-let _ = 
-  parse "var x = 3110 in x + x" 
-  (* - : expr = Let ("x", Int 3110, Binop (Add, Var "x", Var "x")) *)
+(* parse "var x = 3110 in x + x"  *)
+(* - : expr = Let ("x", Int 3110, Binop (Add, Var "x", Var "x")) *)
+
+let is_value : expr -> bool = 
+  function
+  | Int _ | Bool _ -> true
+  | Var _ | Let _ | Binop _ | If _ -> false
+
+let rec subst e v x = 
+  match e with
+  | Var y -> if y = x then v else e
+  | Bool _ -> e
+  | Int _ -> e
+  | Binop (bop, e1, e2) -> Binop (bop, subst e1 v x, subst e2 v x)
+  | Let (y, e1, e2) ->
+    let e1' = subst e1 v x in
+    if x = y then
+      Let (y, e1', e2)
+    else 
+      Let (y, e1', subst e2 v x)
+  | If (e1, e2, e3) ->
+    If (subst e1 v x, subst e2 v x, subst e3 v x) 
+
+let rec step : expr -> expr = function
+  | Int _ | Bool _ -> failwith "Does not step" 
+  | Var _ -> failwith "Unbound variable"
+  | Binop (bop, e1, e2) when is_value e1 && is_value e2 ->
+    step_bop bop e1 e2
+  | Binop (bop, e1, e2) when is_value e1 ->
+    Binop (bop, e1, step e2)
+  | Binop (bop, e1, e2) -> Binop (bop, step e1, e2)
+  | Let (x, e1, e2) when is_value e1 -> subst e2 e1 x
+  | Let (x, e1, e2) -> Let (x, step e1, e2)
+  | If (Bool true, e2, _) -> e2
+  | If (Bool false, _, e3) -> e3
+  | If (Int _, _, _) -> failwith "Guard of if must have type bool"
+  | If (e1, e2, e3) -> If (step e1, e2, e3)
+
+and step_bop bop e1 e2 = 
+  match bop, e1, e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b) 
+  | _ -> failwith "Operator and operand type mismatch"
+
+(* The Multistep Relation *)
+
+(* Of course, in implementing an interpreter, what we really want is to take multiple steps 
+until the expression reaches a value. That is, we want to take as many steps as possible. 
+So, we're interested in the sub-relation e -->* v in which the right-hand side is a value. 
+That's easy to implement: *)
+
+let rec eval_small (e : expr) : expr =
+  if is_value e then 
+    e
+  else 
+    e |> step |> eval_small 
+
+(* Defining the Big-Step Relation 
+
+Binary operators just big-step both of their subexpressions, then apply 
+whatever the primitive operator *)
+
+(* Implementing the Big-Step Relation *)
+
+let rec eval_big (e: expr) : expr =
+  match e with
+  | Int _ | Bool _ -> e
+  | Var _ -> failwith "Unbound value"
+  | Binop (bop, e1, e2) -> eval_bop bop e1 e2
+  | Let (x, e1, e2) -> subst e2 (eval_big e1) x |> eval_big
+  | If (e1, e2, e3) -> eval_if e1 e2 e3
+
+and eval_bop bop e1 e2 =
+  match bop, eval_big e1, eval_big e2 with
+  | Add, Int a, Int b -> Int (a + b)
+  | Mult, Int a, Int b -> Int (a * b)
+  | Leq, Int a, Int b -> Bool (a <= b)
+  | _ -> failwith "Operator and operand type mismatch" 
+
+and eval_if e1 e2 e3 =
+  match eval_big e1 with
+  | Bool true -> eval_big e2
+  | Bool false -> eval_big e3
+  | _ -> failwith "Guard of if must have type bool"
+
