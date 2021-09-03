989b37bb943d2756882cae9bf393b7f34b7aaa18
nogw <gabrielnogueiraoliveira@gmail.com>
2021-09-02 22:09:46 -0300

    interpreter

--- front_end_for_simPL/main.ml
+++ front_end_for_simPL/main.ml
@@ -8,6 +8,66 @@ let parse (s: string) : expr =
 (* parse "var x = 3110 in x + x"  *)
 (* - : expr = Let ("x", Int 3110, Binop (Add, Var "x", Var "x")) *)
 
+type typ = 
+  | TInt
+  | TBool
+
+module type Context = sig
+  type t
+  val empty : t
+  val lookup : t -> string -> typ
+  val extend : t -> string -> typ -> t
+end
+
+module Context: Context = struct
+  type t = (string * typ) list
+  let empty = []
+  let lookup ctx x =
+    try 
+      List.assoc x ctx
+    with Not_found -> failwith "Unbound variable"
+  let extend ctx x ty =
+    (x, ty) :: ctx 
+end
+
+open Context
+
+let rec typeof ctx = function
+  | Int _ -> TInt
+  | Bool _ -> TBool
+  | Var x -> lookup ctx x
+  | Let (x, e1, e2) -> typeof_let ctx x e1 e2
+  | Binop (bop, e1, e2) -> typeof_bop ctx bop e1 e2
+  | If (e1, e2, e3) -> typeof_if ctx e1 e2 e3
+  
+and typeof_let ctx x e1 e2 =
+  let t1 = typeof ctx e1 in
+  let ctx' = extend ctx x t1 in
+  typeof ctx' e2
+
+and typeof_bop ctx bop e1 e2 =
+  let t1, t2 = typeof ctx e1, typeof ctx e2 in  
+  match bop, t1, t2 with
+  | Add, TInt, TInt
+  | Mult, TInt, TInt -> TInt
+  | Leq, TInt, TInt -> TBool
+  | _ -> failwith "Operator and operand type mismatch"
+
+and typeof_if ctx e1 e2 e3 =
+  if typeof ctx e1 = TBool then
+    begin
+      let t2 = typeof ctx e2 in
+      if t2 = typeof ctx e3 then
+        t2
+      else 
+        failwith "Branches of if must have same type"
+    end
+  else 
+    failwith "Guard of if must have type bool"
+
+let typecheck e =
+  ignore (typeof empty e)
+
 let is_value : expr -> bool = 
   function
   | Int _ | Bool _ -> true
@@ -91,3 +151,7 @@ and eval_if e1 e2 e3 =
   | Bool false -> eval_big e3
   | _ -> failwith "Guard of if must have type bool"
 
+let interp_big (s : string) : expr = 
+  let e = parse s in
+  typecheck e;
+  eval_big e
