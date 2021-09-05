d36d7577200f965064bf5315e5a4c30a5aa2770c
nogw <gabrielnogueiraoliveira@gmail.com>
2021-09-05 19:55:34 -0300

    pair parsing

---
+++ exercises_interpreters/pair_parsing/pair_parsing.ml
@@ -0,0 +1,2 @@
+(* in front_end_for_simPL *)
+(* | LPAREN; e1 = expr; COMMA; e2 = expr; RPAREN { Pairs (e1, e2) } *)
---
+++ exercises_interpreters/typing/typing.ml
@@ -0,0 +1,2 @@
+(* var x = 0 in if x <= 1 then 22 else 42 *)
+(* expr = Int 22 *)
